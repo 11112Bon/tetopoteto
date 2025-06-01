37e7766444016a50ae97011fc2f6d33154a1e42a
11112Bon <0924047@gse.okayama-c.ed.jp>
2025-06-01 12:26:17 +0900

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('tetris');
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 context.scale(20, 20);
 
 const holdCanvas = document.getElementById('hold');
